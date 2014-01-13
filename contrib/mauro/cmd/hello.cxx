63194de61bcaea03f64840385f8b9733fc6c3fed
Ricardo Fabbri <rfabbri@gmail.com>
2014-01-13 21:15:10 -0200

    new directory for ric fabbri student Mauro de Amorim

---
+++ contrib/mauro/cmd/hello.cxx
@@ -0,0 +1,13 @@
+// Include standard C++ input/output library
+#include <vcl_iostream.h>
+
+// main is the first function to be called
+int main()
+{
+  // send string "hello world" to the standard
+  // output stream cout.
+  vcl_cout << "Hello world!!!\n";
+
+  // Must return something from main
+  return 0;
+}
---
+++ contrib/mauro/gui/mauro_menu.h
@@ -0,0 +1,21 @@
+// GUI menu for mauro
+
+#ifndef mauro_menu_h_
+#define mauro_menu_h_
+
+#include <vgui/vgui_menu.h>
+
+
+class mauro_menu {
+
+public:
+
+  mauro_menu();
+  virtual ~mauro_menu(){}
+
+  vgui_menu setup_menu();
+
+};
+
+
+#endif //mauro_menu_h_
