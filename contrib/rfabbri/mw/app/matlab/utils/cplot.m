8644e2a6dee9e9152a207379fab11b8ca404d8a8
Ricardo Fabbri <rfabbri@Clima.(none)>
2014-01-21 18:44:03 -0200

    cplot now supports alpha

--- contrib/rfabbri/mw/app/matlab/utils/cplot.m
+++ contrib/rfabbri/mw/app/matlab/utils/cplot.m
@@ -1,6 +1,18 @@
-function h=cplot(v,color)
-  if (nargin ==2)
+function h=cplot(v,color,alpha)
+  if nargin == 3
+    x = v(:,1)';
+    y = v(:,2)';
+    z = v(:,3)';
+
+    xflip = [x(1 : end - 1) fliplr(x)];
+
+    yflip = [y(1 : end - 1) fliplr(y)];
+
+    zflip = [z(1 : end - 1) fliplr(z)];
+
+    h = patch(xflip, yflip, zflip, 'r', 'EdgeAlpha', alpha, 'EdgeColor', color);
+  elseif nargin ==2
     h=plot3(v(:,1),v(:,2),v(:,3),color);
-  else
+  elseif nargin == 1
     h=plot3(v(:,1),v(:,2),v(:,3));
   end
