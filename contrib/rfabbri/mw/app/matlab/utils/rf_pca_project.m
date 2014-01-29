4d30722cfc59aeb75acbbef21e38bd5fa47029c2
Ricardo Fabbri <rfabbri@Clima.(none)>
2014-01-29 16:01:12 -0200

    two spaces for projecting pca

--- contrib/rfabbri/mw/app/matlab/utils/rf_pca_project.m
+++ contrib/rfabbri/mw/app/matlab/utils/rf_pca_project.m
@@ -1,6 +1,3 @@
 function [x_proj_pca, x_proj_orig] = rf_pca_project(x_test, data_db, mean_db)
-  % pca space
-  x_proj_pca = data_db\(x_test-mean_db)
-
-  % original space
-  x_proj_orig = data_db*x_proj_pca + mean_db
+  x_proj_pca = data_db\(x_test-mean_db)       % pca space
+  x_proj_orig = data_db*x_proj_pca + mean_db  % original space
--- contrib/rfabbri/mw/app/matlab/utils/rf_pca_test.m
+++ contrib/rfabbri/mw/app/matlab/utils/rf_pca_test.m
@@ -12,3 +12,33 @@ disp 'result of projection in original space should be [0,0]'
 x_test = [1; 1];
 [x_proj_pca, x_proj_orig] = rf_pca_project(x_test, data_db, mean_db)
 disp 'result of projection in original space should be [1,1]'
+
+x_test = [0.5; 0.5];
+[x_proj_pca, x_proj_orig] = rf_pca_project(x_test, data_db, mean_db)
+disp 'result of projection in original space should be [0.5,0.5]'
+
+x_test = [0.4; 0.6];
+[x_proj_pca, x_proj_orig] = rf_pca_project(x_test, data_db, mean_db)
+disp 'result of projection in original space should be [0.5,0.5]'
+
+% ---------------------------
+% increasing the dimension to 2 should make the projection operator the
+% identity, as the manifold is now the entire plane.
+
+[data_db, mean_db]= rf_pca(data,2)
+
+x_test = [0; 0];
+[x_proj_pca, x_proj_orig] = rf_pca_project(x_test, data_db, mean_db)
+disp 'result of projection in original space should be [0,0]'
+
+x_test = [1; 1];
+[x_proj_pca, x_proj_orig] = rf_pca_project(x_test, data_db, mean_db)
+disp 'result of projection in original space should be [1,1]'
+
+x_test = [0.5; 0.5];
+[x_proj_pca, x_proj_orig] = rf_pca_project(x_test, data_db, mean_db)
+disp 'result of projection in original space should be [0.5,0.5]'
+
+x_test = [0.4; 0.6];
+[x_proj_pca, x_proj_orig] = rf_pca_project(x_test, data_db, mean_db)
+disp 'result of projection in original space should be *** [0.4,0.6] ***' 
