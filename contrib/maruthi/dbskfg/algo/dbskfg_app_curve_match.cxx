a2faee746408c02315d76f8f5ddce21b4f34d81d
Maruthi Narayanan <maruthi.narayanan@gmail.com>
2014-10-09 23:45:50 -0400

    added new files with updated cmake list

---
+++ contrib/maruthi/dbskfg/algo/dbskfg_app_curve_match.cxx
@@ -0,0 +1,81 @@
+#include <dbskfg/algo/dbskfg_app_curve_match.h>
+#include <dbskr/dbskr_scurve.h>
+
+dbskfg_app_curve_match::dbskfg_app_curve_match(
+    vnl_matrix<vl_sift_pix> sift_c1,
+    vnl_matrix<vl_sift_pix> sift_c2)
+    : dbcvr_cvmatch_even(),
+      sift_c1_(sift_c1),
+      sift_c2_(sift_c2)
+{
+    _n1=sift_c1_.cols();
+    _n2=sift_c2_.cols();
+    _template_size=5;
+}
+
+
+dbskfg_app_curve_match::~dbskfg_app_curve_match() 
+{ 
+    sift_c1_.clear();
+    sift_c2_.clear();
+    clear();
+}
+
+void dbskfg_app_curve_match::initializeDPCosts()
+{
+    _DPCost.clear();
+    _DPMap.clear();
+  
+    assert (_n1>0);
+    assert (_n2>0);
+
+    for (int i=0;i<_n1;i++) 
+    {
+        vcl_vector<double> tmp1(_n2,DP_VERY_LARGE_COST);
+        _DPCost.push_back(tmp1);
+        vcl_pair <int,int> tmp3(0,0);
+        vcl_vector< vcl_pair <int,int> > tmp2(_n2,tmp3);
+        _DPMap.push_back(tmp2);
+    }
+
+    //Cost Matrix Initialization
+    _finalCost = DP_VERY_LARGE_COST;
+    for (int n=0;n<_n1;n++) 
+    {
+        for (int m=0;m<_n2;m++) 
+        {
+            _DPCost[n][m]=DP_VERY_LARGE_COST;
+        }
+    }
+    _DPCost[0][0]=0.0;
+
+}
+
+// Cost of matching the  interval [x(i),x(ip)] to the interval [y(j),y(jp)].
+double dbskfg_app_curve_match::computeIntervalCost(int i, int ip, int j, int jp) 
+{
+
+    double dA1_cont(0.0),dA2_cont(0.0);
+
+    vnl_vector<vl_sift_pix>  sift_i  = sift_c1_.get_column(i);
+    vnl_vector<vl_sift_pix>  sift_ip = sift_c1_.get_column(ip);
+    double dapp1=descr_cost(sift_i,sift_ip);
+
+    vnl_vector<vl_sift_pix>  sift_j = sift_c2_.get_column(j);
+    vnl_vector<vl_sift_pix>  sift_jp= sift_c2_.get_column(jp);
+    double dapp2=descr_cost(sift_j,sift_jp);
+    
+
+    double app_cont=vcl_fabs(dapp1-dapp2);
+
+    double app_diff=vcl_fabs(descr_cost(sift_i,sift_j)+
+                             descr_cost(sift_ip,sift_jp));
+
+    double cost=app_cont+app_diff;
+
+    return cost;
+} 
+
+
+
+
---
+++ contrib/maruthi/dbskfg/algo/dbskfg_app_curve_match.h
@@ -0,0 +1,69 @@
+//---------------------------------------------------------------------
+// This is contrib/maruthi/dbskfg/alog/dbskfg_app_curve_match.h
+//:
+// \file
+// \brief This is a curve matching class subclassed from dbcvr_cvmatch_even
+//        to match appearance for scurves
+//        Dynamic programming core is the same with dbcvr curve matching
+//        interval cost computation is different and specific to shock curves.
+//
+// \author
+//  Maruthi Narayanan - October 9th , 2014
+//
+//-------------------------------------------------------------------------
+#ifndef _dbskfg_app_curve_match_h
+#define _dbskfg_app_curve_match_h
+
+#include <dbcvr/dbcvr_cvmatch_even.h>
+#include <vnl/vnl_matrix.h>
+#include <vl/sift.h>
+#include <vnl/vnl_vector.h>
+
+extern "C" {
+#include <vl/mathop.h>
+}
+
+class dbskfg_app_curve_match : public dbcvr_cvmatch_even 
+{
+public:
+    
+    dbskfg_app_curve_match(vnl_matrix<vl_sift_pix> sift_c1,
+                           vnl_matrix<vl_sift_pix> sift_c2);
+    
+    virtual ~dbskfg_app_curve_match();
+    
+    //overwritten DP functions
+    virtual void initializeDPCosts();
+    virtual double computeIntervalCost (int i, int ip, int j, int jp);
+    
+    
+protected:
+    
+    //shock curves to match
+    vnl_matrix<vl_sift_pix> sift_c1_;
+    vnl_matrix<vl_sift_pix> sift_c2_;
+
+ 
+    double descr_cost(vnl_vector<vl_sift_pix>& v1,
+                      vnl_vector<vl_sift_pix>& v2)
+    {
+        VlFloatVectorComparisonFunction Chi2_distance =    
+            vl_get_vector_comparison_function_f (VlDistanceChi2) ;
+
+
+        vl_sift_pix result_final[1];
+
+        vl_eval_vector_comparison_on_all_pairs_f(result_final,
+                                                 v1.size(),
+                                                 v1.data_block(),
+                                                 1,
+                                                 v2.data_block(),
+                                                 1,
+                                                 Chi2_distance);
+        
+        return result_final[0];
+    }
+};
+
+
+#endif // _dbskfg_app_curve_match_h
