f46b77b4b402250c72dc5131d8e55ff6a08b4752
Maruthi Narayanan <maruthi.narayanan@gmail.com>
2015-01-28 19:22:01 -0500

    updated tester sift class

--- contrib/maruthi/con_graph/sift_compute.cxx
+++ contrib/maruthi/con_graph/sift_compute.cxx
@@ -12,6 +12,70 @@
 
 
 
+void compute_grad_maps(vil_image_resource_sptr& input_image,
+                       vl_sift_pix** grad_data,
+                       VlSiftFilt** filter);
+
+
+void compute_grad_maps(vil_image_resource_sptr& input_image,
+                       vl_sift_pix** grad_data,
+                       VlSiftFilt** filter)
+{
+
+    vil_image_view<vxl_byte> temp = 
+        vil_convert_to_grey_using_rgb_weighting(
+            0.2989,
+            0.5870,
+            0.1140,
+            input_image->get_view());
+    
+    vil_image_view<double> orig_image;
+    vil_convert_cast(temp,orig_image);
+
+    unsigned int width  = orig_image.ni();
+    unsigned int height = orig_image.nj();
+
+    double* gradient_magnitude = (double*) 
+        vl_malloc(width*height*sizeof(double));
+    double* gradient_angle     = (double*) 
+        vl_malloc(width*height*sizeof(double));
+
+    double* orig_image_data=orig_image.top_left_ptr();
+
+    vl_imgradient_polar_d(
+        gradient_magnitude, // gradient magnitude 
+        gradient_angle,     // gradient angle
+        1,                  // output width
+        width,              // output height
+        orig_image_data,    // input image
+        width,              // input image width
+        height,             // input image height
+        width);             // input image stride
+
+    *filter   = vl_sift_new(width,height,-1,-1,0);
+    *grad_data= (vl_sift_pix*) 
+        vl_malloc(sizeof(vl_sift_pix)*width*height*2);
+   
+    unsigned int index=0;
+    for ( unsigned int i=0; i < width*height; ++i)
+    {
+        double mag  = gradient_magnitude[i];
+        double angle= gradient_angle[i];
+
+        (*grad_data)[index]=mag;
+        ++index;
+        (*grad_data)[index]=angle;
+        ++index;
+    }
+
+    vl_free(gradient_magnitude);
+    vl_free(gradient_angle);
+
+}
+
+
+
+
 int main( int argc, char *argv[] )
 {
 
@@ -43,60 +107,17 @@ int main( int argc, char *argv[] )
     vil_image_resource_sptr img_sptr = 
         vil_load_image_resource(input_img.c_str());
 
-    vil_image_view<vxl_byte> temp = 
-        vil_convert_to_grey_using_rgb_weighting(
-            0.2989,
-            0.5870,
-            0.1140,
-            img_sptr->get_view());
-    
-    vil_image_view<double> orig_image;
-    vil_convert_cast(temp,orig_image);
+    vl_sift_pix* grad_data(0);
+    VlSiftFilt* filter(0);
 
-    unsigned int width  = orig_image.ni();
-    unsigned int height = orig_image.nj();
+    compute_grad_maps(img_sptr,
+                      &grad_data,
+                      &filter);
 
-    double* gradient_magnitude = (double*) 
-        vl_malloc(width*height*sizeof(double));
-    double* gradient_angle     = (double*) 
-        vl_malloc(width*height*sizeof(double));
-
-    double* orig_image_data=orig_image.top_left_ptr();
-
-    vl_imgradient_polar_d(
-        gradient_magnitude, // gradient magnitude 
-        gradient_angle,     // gradient angle
-        1,                  // output width
-        width,              // output height
-        orig_image_data,    // input image
-        width,              // input image width
-        height,             // input image height
-        width);             // input image stride
-
-    VlSiftFilt* filter = vl_sift_new(width,height,-1,-1,0);
-    vl_sift_pix* grad_data=(vl_sift_pix*) 
-        vl_malloc(sizeof(vl_sift_pix)*width*height*2);
     vl_sift_set_magnif(filter,1.0);
-
-    unsigned int index=0;
-    for ( unsigned int i=0; i < width*height; ++i)
-    {
-        double mag  = gradient_magnitude[i];
-        double angle= gradient_angle[i];
-
-        (grad_data)[index]=mag;
-        ++index;
-        (grad_data)[index]=angle;
-        ++index;
-    }
-
-    vl_free(gradient_magnitude);
-    vl_free(gradient_angle);
-
 
     vl_sift_pix descr_ps1[128];
     memset(descr_ps1, 0, sizeof(vl_sift_pix)*128);
-
 
     vl_sift_calc_raw_descriptor(filter,
                                 grad_data,
@@ -116,5 +137,8 @@ int main( int argc, char *argv[] )
         vcl_cout<<512*descr_ps1[i]<<vcl_endl;
 
     }
+
+    vl_free(filter);
+    vl_free(grad_data);
     return 0;
 }
