8231ae6a7fc7b3b0e7bd01a376f5db3967cc5373
Ricardo Fabbri <rfabbri@lems.brown.edu>
2015-09-02 00:12:13 -0300

    sketching script to output cameras one by one

---
+++ contrib/rfabbri/mw/scripts/blender_cameras.py
@@ -0,0 +1,11 @@
+#
+# Generate ground truth data from ground truth blender file
+#
+# For instance, open the project
+#
+# /Users/rfabbri/3d-curve-drawing/ground-truth/models/pabellon_barcelona_v1/pavillon_barcelone_v1.2-cam-002.blend
+#
+# Then select the desired animated camera and set as active camera
+#
+# Then run this script. It will play frame by frame and export the 3x4 camera
+# matrix for that frame, among other things.
