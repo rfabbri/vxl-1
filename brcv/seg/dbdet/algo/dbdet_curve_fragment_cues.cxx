4c0e044fba58512b4155554bb11c187df1b1b4ce
Ricardo Fabbri <rfabbri@gmail.com>
2016-05-26 21:47:12 -0300

    about to compute edge sparsity

---
+++ brcv/seg/dbdet/algo/dbdet_curve_fragment_cues.cxx
@@ -0,0 +1,67 @@
+#include <dbdet_curve_fragment_cues.h>
+
+void
+dbdet_curve_fragment_cues(
+    const dbdet_edgel_chain &c,
+    const vil_image_view<rgbP >&hsv,
+    y_feature_vector *features_ptr // indexed by the enum
+    )
+{
+  static unsigned local_dist = 1; // distance used for local sampling
+  y_feature_vector &features = *features_ptr;
+
+  // curvature
+  vnl_vector<double> k;
+  dbgl_compute_curvature(c, &k);
+
+  for (unsigned i=0; i < k.size(); ++i)
+    if (vnl_math::isnan(k[i]))
+      k[i] = 0;
+
+  { // wiggliness is the number of times curvature changes sign
+    features[Y_WIGG] = 0;
+    for (unsigned i=0; i + 1 < k.size(); ++i) {
+      features[Y_WIGG] += ( (k[i+1] >= 0) == (k[i] >= 0) );
+    }
+    features[Y_WIGG] /= k.size();
+  }
+
+  features[Y_ABS_K] = k.one_norm() / k.size();
+
+  { // HSV gradients
+    // examine local_dist neighborhood around curve along normals
+    vcl_vector< vnl_vector_fixed<double, 2> > n;
+    n.reserve(k.size());
+    dbgl_compute_normals(c, &n);
+
+    // get neighborhood points to be examined
+    
+
+    features[Y_HUE_GRAD] = 0;
+    features[Y_SAT_GRAD] = 0;
+    features[Y_BG_GRAD]  = 0;
+
+    for (unsigned i=0; i < c.size(); ++i) {
+      left_x  = c[i].x() - local_dist * n[i][0];
+      left_y  = c[i].y() - local_dist * n[i][1];
+      right_x = c[i].x() + local_dist * n[i][0];
+      right_y = c[i].y() + local_dist * n[i][1];
+
+      // make sure image clamps to within bounds
+      vil_border_accessor<vil_image_view<rgbP> >
+        hsv_clamped = vil_border_create_accessor(hsv,vil_border_create_geodesic(hsv));
+
+      // TODO test if imae indexing is (x,y) or (y,x)
+      features[Y_HUE_GRAD] += vnl::abs(hsv_clamped(left_x,left_y) - hsv_clamped(right_x,right_y));
+      features[Y_SAT_GRAD] += vnl::abs(hsv_clamped(left_x,left_y) - hsv_clamped(right_x,right_y));
+      features[Y_BG_GRAD] += vnl::abs(hsv_clamped(left_x,left_y) - hsv_clamped(right_x,right_y));
+    }
+    features[Y_HUE_GRAD] /= c.size();
+    features[Y_SAT_GRAD] = c.size();
+    features[Y_BG_GRAD]  = c.size();
+  }
+
+  { // lateral edge sparsity
+
+  }
+}
