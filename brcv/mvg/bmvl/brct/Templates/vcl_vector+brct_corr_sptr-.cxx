d942ead6e857ec54c2d2e7b97659efed8fa8cf76
Ricardo Fabbri <rfabbri@gmail.com>
2016-09-12 20:08:04 -0300

    vcl_vector instantiate removed for latest VXL

--- brcv/mvg/bmvl/brct/Templates/vcl_vector+brct_corr_sptr-.cxx
+++
@@ -1,5 +0,0 @@
-#include <brct/brct_corr_sptr.h>
-#include <vcl_vector.hxx>
-
-VCL_VECTOR_INSTANTIATE(brct_corr_sptr);
-
--- brcv/mvg/bmvl/brct/Templates/vcl_vector+brct_plane_corr_sptr-.cxx
+++
@@ -1,5 +0,0 @@
-#include <brct/brct_plane_corr_sptr.h>
-#include <vcl_vector.hxx>
-
-VCL_VECTOR_INSTANTIATE(brct_plane_corr_sptr);
-
