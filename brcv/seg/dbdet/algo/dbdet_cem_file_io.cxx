fb7cf290264c52c43beeb35485c58317f7e02603
Caio SOUZA <caioc2bolado@gmail.com>
2016-09-28 14:49:37 -0300

    CEM load fix, without changing behavior of old code using it

--- brcv/seg/dbdet/algo/dbdet_cem_file_io.cxx
+++ brcv/seg/dbdet/algo/dbdet_cem_file_io.cxx
@@ -7,7 +7,7 @@
 
 // -----------------------------------------------------------------------------
 //: Save the contour fragment graph as a .cem file
-bool dbdet_save_cem(vcl_string filename, dbdet_edgemap_sptr EM, dbdet_curve_fragment_graph& CFG)
+bool dbdet_save_cem(vcl_string filename, dbdet_edgemap_sptr EM, dbdet_curve_fragment_graph& CFG, bool duplicate_edgel = false)
 {
   //1)If file open fails, return.
   vcl_ofstream outfp(filename.c_str(), vcl_ios::out);
@@ -108,7 +108,7 @@ bool dbdet_save_cem(vcl_string filename, dbdet_edgemap_sptr EM, dbdet_curve_frag
 
 // -----------------------------------------------------------------------------
 //: load the older version of the cem file (for backward compatibility)
-dbdet_edgemap_sptr dbdet_load_cem_v1(vcl_ifstream &infp, dbdet_curve_fragment_graph& CFG, int width, int height, bool convert_degrees_to_radians)
+dbdet_edgemap_sptr dbdet_load_cem_v1(vcl_ifstream &infp, dbdet_curve_fragment_graph& CFG, int width, int height, bool convert_degrees_to_radians, bool duplicate_edgel)
 {
   char lineBuffer[1024];
   int numContours, numTotalEdges, numEdges;
@@ -167,8 +167,15 @@ dbdet_edgemap_sptr dbdet_load_cem_v1(vcl_ifstream &infp, dbdet_curve_fragment_gr
         dbdet_edgel* e = new dbdet_edgel(vgl_point_2d<double>(x,y), dir, conf);
         e->id = edgemap->edgels.size();
         edgemap->insert(e);
-
-        chain->push_back(e);
+        
+        if (duplicate_edgel)
+        {
+          //Duplicate edgels, CFG should not depend on the edgemap itself?
+          dbdet_edgel * e2 =  new dbdet_edgel(*e);
+          chain->push_back(e2);
+        }
+        else
+          chain->push_back(e);
       }
 
       //go to the end of the block
@@ -196,7 +203,7 @@ dbdet_edgemap_sptr dbdet_load_cem_v1(vcl_ifstream &infp, dbdet_curve_fragment_gr
 
 // -----------------------------------------------------------------------------
 //: Loads an ascii file containing a graph of edgel chains (the contour fragment graph)
-dbdet_edgemap_sptr dbdet_load_cem(vcl_string filename, dbdet_curve_fragment_graph& CFG)
+dbdet_edgemap_sptr dbdet_load_cem(vcl_string filename, dbdet_curve_fragment_graph& CFG, bool duplicate_edgel)
 {
   ////
   char lineBuffer[1024];
@@ -216,12 +223,12 @@ dbdet_edgemap_sptr dbdet_load_cem(vcl_string filename, dbdet_curve_fragment_grap
   if (!vcl_strncmp(lineBuffer, ".CEM v2.0", sizeof(".CEM v2.0")-1))
   {
     version =2;
-    edgemap = dbdet_load_cem_v2(infp, CFG);
+    edgemap = dbdet_load_cem_v2(infp, CFG, duplicate_edgel);
     vcl_cout << "Loaded: " << filename.c_str() << ".\n";
     return edgemap;
   }
   else {
-    edgemap = dbdet_load_cem_v1(infp, CFG);
+    edgemap = dbdet_load_cem_v1(infp, CFG, duplicate_edgel);
     vcl_cout << "Loaded: " << filename.c_str() << ".\n";
     return edgemap;
   }  
@@ -235,7 +242,7 @@ dbdet_edgemap_sptr dbdet_load_cem(vcl_string filename, dbdet_curve_fragment_grap
 
 // -----------------------------------------------------------------------------
 //: load cem file version 2
-dbdet_edgemap_sptr dbdet_load_cem_v2(vcl_ifstream &infp, dbdet_curve_fragment_graph& CFG)
+dbdet_edgemap_sptr dbdet_load_cem_v2(vcl_ifstream &infp, dbdet_curve_fragment_graph& CFG, bool duplicate_edgel)
 {
   //
   char lineBuffer[1024];
@@ -310,9 +317,14 @@ dbdet_edgemap_sptr dbdet_load_cem_v2(vcl_ifstream &infp, dbdet_curve_fragment_gr
         infp >> e_id;
         
         while (!infp.fail()){ //read in all the ids until the end bracket is reached
-          //Duplicate edgels, CFG should not depend on the edgemap itself?
-          dbdet_edgel * e =  new dbdet_edgel(*edgemap->edgels[e_id]);
-          chain->push_back(e);
+          if (duplicate_edgel)
+          {
+            //Duplicate edgels, CFG should not depend on the edgemap itself?
+            dbdet_edgel * e =  new dbdet_edgel(*edgemap->edgels[e_id]);
+            chain->push_back(e);
+          }
+          else
+            chain->push_back(edgemap->edgels[e_id]);
           infp >> e_id;
         }
         infp.clear(); //clear the fail bit
