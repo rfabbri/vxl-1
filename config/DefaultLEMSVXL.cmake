2124c608d21b8544ba9e5b54c498775d36aed123
Ricardo Fabbri <rfabbri@gmail.com>
2017-01-06 13:02:03 -0200

    default vxl and vxd locations in cmake defaults

--- config/DefaultLEMSVXL.cmake
+++ config/DefaultLEMSVXL.cmake
@@ -4,6 +4,8 @@ endif()
 set(BUILD_SHARED_LIBS ON CACHE BOOL "Reduce link time by using shared libs in LEMSVPE")
 set(CMAKE_BUILD_TYPE "Debug" CACHE STRING "Build Debug by default in LEMSVPE")
 set(BUILD_CONTRIB ON CACHE BOOL "Build LEMS contrib area (eg, for edge_det and rfabbri/mw)")
+set(VXL_DIR "../vxl-bin" CACHE STRING "Location of VXL build in LEMSVPE")
+set(VXD_DIR "../vxd-bin" CACHE STRING "Location of VXD build in LEMSVPE")
 
 # can be useful to set USE_BOOST 
 
--- config/DefaultVXD.cmake
+++ config/DefaultVXD.cmake
@@ -3,5 +3,6 @@ if(APPLE)
 endif()
 set(BUILD_SHARED_LIBS ON CACHE BOOL "Reduce link time by using shared libs in LEMSVPE")
 set(CMAKE_BUILD_TYPE "Debug" CACHE STRING "Build Debug by default in LEMSVPE")
+set(VXL_DIR "../vxl-bin" CACHE STRING "Location of VXL build in LEMSVPE")
 
 message("Try to compile core first, then the libs you need.")
