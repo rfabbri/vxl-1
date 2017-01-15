d15de273e4bcc9f6369eaa155f54b6c39baebb78
Caio SOUZA <caioc2bolado@gmail.com>
2017-01-15 13:30:11 -0200

    Updated contour merge

--- brcv/seg/dbdet/algo/dbdet_graphical_model_contour_merge.h
+++ brcv/seg/dbdet/algo/dbdet_graphical_model_contour_merge.h
@@ -32,7 +32,7 @@ public:
     temp_n.reserve(nbr_num_edges);
   }
 
-  void dbdet_merge_contour(dbdet_curve_fragment_graph & CFG, y_params_1_vector & beta1, y_params_1_vector & fmean1, y_params_0_vector & beta0, y_params_0_vector & fmean0);
+  void dbdet_merge_contour(dbdet_curve_fragment_graph & CFG, y_params_1_vector & beta1, y_params_1_vector & fmean1, y_params_0_vector & beta0, y_params_0_vector & fmean0, dbdet_curve_fragment_graph & newCFG);
 private:
 
   void dbdet_degree_2_node_cues(dbdet_edgel_chain & c1, dbdet_edgel_chain & c2, double & geom_diff, double & tex_diff, bool invert = true);
@@ -40,6 +40,9 @@ private:
   void dbdet_merge_at_degree_2_node(dbdet_factor_graph & G, unsigned c1_id, unsigned c2_id, unsigned g_idx, unsigned edgel_id);
 
   void compute_texture_hist(dbdet_edgel_chain & chain, vcl_vector< vnl_vector_fixed<double, 2> > & n, y_hist_vector & left, y_hist_vector & right);
+
+ //Copy first level objects of CFG into newCFG
+  void deep_copy_cfg(dbdet_curve_fragment_graph & CFG, dbdet_curve_fragment_graph & newCFG);
 
   unsigned ni() const { return em_.ncols(); }
   unsigned nj() const { return em_.nrows(); }
