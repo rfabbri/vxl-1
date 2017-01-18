a1f8f3140f5540398afdd4149954bc747fdef4ef
Ricardo Fabbri <rfabbri@gmail.com>
2017-01-18 20:31:02 -0200

    dbsk2d: added back and resolved instantiation

---
+++ lemsvxl/brcv/shp/dbsk2d/Templates/vtol_list_functions+dbsk2d_bnd_vertex~-.cxx
@@ -0,0 +1,18 @@
+// This is file shp/dbsk2d/Templates/vtol_list_functions+dbsk2d_bnd_vertex~-.cxx
+
+#include <dbsk2d/dbsk2d_bnd_vertex.h>
+#include <dbsk2d/dbsk2d_bnd_vertex_sptr.h>
+
+#include <vtol/vtol_list_functions.hxx>
+#include <vcl_vector.h>
+#include <vcl_list.h>
+
+template vcl_vector<dbsk2d_bnd_vertex* >* tagged_union(vcl_vector<dbsk2d_bnd_vertex*>*);
+
+template vcl_vector<dbsk2d_bnd_vertex_sptr >* tagged_union(vcl_vector<dbsk2d_bnd_vertex_sptr>* );
+
+template vcl_list<dbsk2d_bnd_vertex* >* tagged_union(vcl_list<dbsk2d_bnd_vertex*>*);
+
+template vcl_list<dbsk2d_bnd_vertex_sptr >* tagged_union(vcl_list<dbsk2d_bnd_vertex_sptr>* );
+
+
