78b1fa244c3df5d76689c7bffc74d0e114e82a76
Ricardo Fabbri <rfabbri@gmail.com>
2017-01-24 16:29:41 -0200

    lemsvpe vxd defaults: build unmaintained

--- config/DefaultVXD.cmake
+++ config/DefaultVXD.cmake
@@ -4,5 +4,6 @@ endif()
 set(BUILD_SHARED_LIBS ON CACHE BOOL "Reduce link time by using shared libs in LEMSVPE")
 set(CMAKE_BUILD_TYPE "Debug" CACHE STRING "Build Debug by default in LEMSVPE")
 set(VXL_DIR "../vxl-bin" CACHE STRING "Location of VXL build in LEMSVPE")
+set(BUILD_UNMAINTAINED_LIBRARIES ON CACHE BOOL "Build libraries that are no longer actively maintained in LEMSVPE")
 
 message("Try to compile core first, then the libs you need.")
