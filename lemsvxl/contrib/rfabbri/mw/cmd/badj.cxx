953c97a1c584c4c1c7706daa7383b79eec152d57
Ricardo Fabbri <rfabbri@gmail.com>
2017-07-06 09:35:06 -0300

    badj seems ok

--- lemsvxl/contrib/rfabbri/mw/cmd/badj.cxx
+++ lemsvxl/contrib/rfabbri/mw/cmd/badj.cxx
@@ -29,58 +29,60 @@ void get_corrs(
   unsigned npts=10;
   std::vector< std::vector<vgl_point_2d<double> > > imgpts_percam = *pimgpts_percam;
 
-  imgpts[0].resize(npts);
-  imgpts[0][0].set(891.0, 460.0);
-  imgpts[0][1].set(827.0, 550.0);
-  imgpts[0][2].set(826.0, 732.0);
-  imgpts[0][3].set(961.0, 734.0);
-  imgpts[0][4].set(961.0, 550.0);
-  imgpts[0][5].set(1026.0, 463.0);
-  imgpts[0][6].set(1059.0, 463.0);
-  imgpts[0][7].set(1043, 629);
-  imgpts[0][8].set(1063, 700);
-  imgpts[0][9].set(1025, 700);
+  imgpts_percam[0].resize(npts);
+  imgpts_percam[0][0].set(891.0, 460.0);
+  imgpts_percam[0][1].set(827.0, 550.0);
+  imgpts_percam[0][2].set(826.0, 732.0);
+  imgpts_percam[0][3].set(961.0, 734.0);
+  imgpts_percam[0][4].set(961.0, 550.0);
+  imgpts_percam[0][5].set(1026.0, 463.0);
+  imgpts_percam[0][6].set(1059.0, 463.0);
+  imgpts_percam[0][7].set(1043, 629);
+  imgpts_percam[0][8].set(1063, 700);
+  imgpts_percam[0][9].set(1025, 700);
 
-  imgpts[1].resize(npts);
-  imgpts[1][0].set(1011.5, 548.5);
-  imgpts[1][1].set(961.5, 635.2);
-  imgpts[1][2].set(958.5, 812.0);
-  imgpts[1][3].set(1048.5, 816.2);
-  imgpts[1][4].set(1051, 637);
-  imgpts[1][5].set(1100, 552);
-  imgpts[1][6].set(1125, 552);
-  imgpts[1][7].set(1111, 716);
-  imgpts[1][8].set(1126.5, 785);
-  imgpts[1][9].set(1101, 784);
+  imgpts_percam[1].resize(npts);
+  imgpts_percam[1][0].set(1011.5, 548.5);
+  imgpts_percam[1][1].set(961.5, 635.2);
+  imgpts_percam[1][2].set(958.5, 812.0);
+  imgpts_percam[1][3].set(1048.5, 816.2);
+  imgpts_percam[1][4].set(1051, 637);
+  imgpts_percam[1][5].set(1100, 552);
+  imgpts_percam[1][6].set(1125, 552);
+  imgpts_percam[1][7].set(1111, 716);
+  imgpts_percam[1][8].set(1126.5, 785);
+  imgpts_percam[1][9].set(1101, 784);
 
-  imgpts[2].resize(npts);
-  imgpts[2][0].set(923, 81);
-  imgpts[2][1].set(854.5, 178.8);
-  imgpts[2][2].set(856, 373);
-  imgpts[2][3].set(1000, 372);
-  imgpts[2][4].set(999, 178);
-  imgpts[2][5].set(1070, 84);
-  imgpts[2][6].set(1102, 82);
-  imgpts[2][7].set(1087, 262);
-  imgpts[2][8].set(1108, 333);
-  imgpts[2][9].set(1067, 334);
+  imgpts_percam[2].resize(npts);
+  imgpts_percam[2][0].set(923, 81);
+  imgpts_percam[2][1].set(854.5, 178.8);
+  imgpts_percam[2][2].set(856, 373);
+  imgpts_percam[2][3].set(1000, 372);
+  imgpts_percam[2][4].set(999, 178);
+  imgpts_percam[2][5].set(1070, 84);
+  imgpts_percam[2][6].set(1102, 82);
+  imgpts_percam[2][7].set(1087, 262);
+  imgpts_percam[2][8].set(1108, 333);
+  imgpts_percam[2][9].set(1067, 334);
 
-  imgpts[3].resize(npts);
-  imgpts[3][0].set(1069, 379);
-  imgpts[3][1].set(1018, 472.6);
-  imgpts[3][2].set(1018.5, 661);
-  imgpts[3][3].set(1115, 662);
-  imgpts[3][4].set(1114, 472);
-  imgpts[3][5].set(1165, 380);
-  imgpts[3][6].set(1191, 380);
-  imgpts[3][7].set(1179, 603);
-  imgpts[3][8].set(1194, 624);
-  imgpts[3][9].set(1168, 624);
+  imgpts_percam[3].resize(npts);
+  imgpts_percam[3][0].set(1069, 379);
+  imgpts_percam[3][1].set(1018, 472.6);
+  imgpts_percam[3][2].set(1018.5, 661);
+  imgpts_percam[3][3].set(1115, 662);
+  imgpts_percam[3][4].set(1114, 472);
+  imgpts_percam[3][5].set(1165, 380);
+  imgpts_percam[3][6].set(1191, 380);
+  imgpts_percam[3][7].set(1179, 603);
+  imgpts_percam[3][8].set(1194, 624);
+  imgpts_percam[3][9].set(1168, 624);
 
 
   for (unsigned c = 0; c < ncams; ++c)
-    for (unsigned pw=0; pw < npts; ++i)
+    for (unsigned pw=0; pw < npts; ++pw) {
+      assert(imgpts_percam[c].size() == npts);
       image_points_linearlist->push_back(imgpts_percam[c][pw]);
+    }
 
   // make the mask (using all the points)
   *mask = std::vector<std::vector<bool> > (ncams, std::vector<bool>(npts,true) );
@@ -93,7 +95,7 @@ void initialize_world_by_triangulation(
     std::vector<vgl_point_3d<double> > *world)
 {
   // assume all points show up in all cams
-  npts = imgspts[0].size();
+  npts = imgpts[0].size();
   *world = std::vector<vgl_point_3d<double> >(npts,vgl_point_3d<double>(0.0, 0.0, 0.0));
 }
 
@@ -119,10 +121,12 @@ int main(int argc, char** argv)
   std::vector<vpgl_perspective_camera<double> > ini_cams;
   get_cams(a_cams.value_, &ini_cams);
 
-  std::vector<vgl_point_2d<double> > image_points_linearlist; 
+  std::vector<vgl_point_2d<double> > imgpts_linearlist; 
   std::vector< std::vector<vgl_point_2d<double> > > imgpts_percam;
-  get_corrs(ini_cams.size(), &image_points_linearlist, &imgpts_percam, &mask);
+  std::vector<std::vector<bool> > mask;
+  get_corrs(ini_cams.size(), &imgpts_linearlist, &imgpts_percam, &mask);
 
+  std::vector<vgl_point_3d<double> > ini_world;
   initialize_world_by_triangulation(imgpts_percam, ini_cams, &ini_world);
  
   // write a VRML version of the scene for debugging
@@ -137,7 +141,7 @@ int main(int argc, char** argv)
 
     vpgl_bundle_adjust ba;
     // ba.set_use_gradient(false);
-    bool converge = ba.optimize(unknown_cameras, unknown_world, image_points, mask);
+    bool converge = ba.optimize(unknown_cameras, unknown_world, imgpts_linearlist, mask);
     std::cout << "Converged? " << converge << std::endl;
     vpgl_bundle_adjust::write_vrml("badj_fixed_k.wrl", unknown_cameras, unknown_world);
     write_cams(unknown_cameras);
