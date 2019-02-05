c4f7c9a4ae46d75a48c86d8720763b3fa58c415c
Ricardo Fabbri <rfabbri@gmail.com>
2019-02-05 08:31:20 -0500

    displaying cams normally for synthsphere

--- lemsvxl/contrib/rfabbri/mw/app/matlab/data-handling/synthetic_data_ascii_show.m
+++ lemsvxl/contrib/rfabbri/mw/app/matlab/data-handling/synthetic_data_ascii_show.m
@@ -1,3 +1,5 @@
+% Data read -----------------------------------------------
+clear
 mydir="~/data/rfabbri/out-tmp/";
 fname = dir(mydir + '*.extrinsic');
 nviews = length(fname);
@@ -5,4 +7,20 @@ extrinsics = zeros(4,3,nviews);
 
 for i=1:nviews
   extrinsics(:,:,i) = load(mydir + fname(i).name);
+end
+
+C = squeeze(extrinsics(4,:,:))';
+R = extrinsics(1:3,:,:);
+
+
+% Display -----------------------------------------------
+pts3d = load(mydir+'crv-3D-pts.txt');
+cplot(pts3d,'.');
+hold on
+axis equal
+cplot(C,'o');
+
+
+for i=1:nviews
+  hh(i)=plotCamera('Location',C(i,:),'Orientation',R(:,:,i),'AxesVisible',true,'Opacity',0,'Size',50);
 end
