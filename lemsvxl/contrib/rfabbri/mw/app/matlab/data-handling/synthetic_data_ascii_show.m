df2d7437b8070e2e7d15f7d77868999049615a81
Ricardo Fabbri <rfabbri@gmail.com>
2019-02-06 03:30:18 -0500

    showing selected cams

--- lemsvxl/contrib/rfabbri/mw/app/matlab/data-handling/synthetic_data_ascii_show.m
+++ lemsvxl/contrib/rfabbri/mw/app/matlab/data-handling/synthetic_data_ascii_show.m
@@ -30,12 +30,24 @@ hold on
 axis equal
 %cplot(C,'o');
 
+selected_ids = [55 43 63]; % subtract 1 to get file id!
 
 for i=1:nviews
-  hh(i)=plotCamera('Location',C(i,:),'Orientation',R(:,:,i),'AxesVisible',true,'Opacity',0,'Size',50);
+  hh(i)=plotCamera('Location',C(i,:),'Orientation',R(:,:,i),'AxesVisible',false,'Opacity',0.8,'Size',30,'Label',num2str(i));
+%  hh(i).Visible = false;
   % show z axes
   % cplot([C(i,:); C(i,:) + 5000*R(3,:,i)]);
+  for sid=1:length(selected_ids)
+    if i == selected_ids(sid)
+      hh(i).Visible = true;
+    end
+  end
+end
+for sid=1:length(selected_ids)
+  disp ['selecting' num2str(selected_ids(sid))]
+  hh(selected_ids(sid)).Color = [0.5 0.8 0];
+  % show z axes
+  cplot([C(selected_ids(sid),:); C(selected_ids(sid),:) + 2500*R(3,:,selected_ids(sid))]);
 end
 
-
-
+%r
