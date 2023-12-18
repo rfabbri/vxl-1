24ac08dfb3b56080490899c1ab6ef66ca9622f67
Ricardo Fabbri <rfabbri@gmail.com>
2023-12-17 21:36:44 -0300

    [3DCD] enchanced curve sketch from anil - using _e suffix

---
+++ vxd/contrib/brld/bmvgd/bmcsd/bmcsd_curve_3d_sketch_e.cxx
@@ -0,0 +1,93 @@
+#include "bmcsd_curve_3d_sketch_e.h"
+#include <vul/vul_file.h>
+#include <vul/vul_file.h>
+#include <vsl/vsl_vector_io.h>
+
+
+bool bmcsd_curve_3d_sketch_e::
+write_dir_format(std::string dir) const
+{
+  vul_file::make_directory(dir);
+
+  { // version number 
+    std::ofstream f((dir + std::string("/version")).c_str());
+    f << dir_format_version() << std::endl;
+  }
+  { // number 
+    std::ofstream f((dir + std::string("/num_curves")).c_str());
+    f << num_curves() << std::endl;
+  }
+
+  // Write out all curves in binary format. In the future, we might write in
+  // another format.
+  vul_file::make_directory(dir+"/crvs");
+  bool retval = mywritev(dir + "/crvs/rec", ".dat", curves_3d());
+  if(!retval) {
+    std::cerr << "Error while trying to write 3D curves to file.\n";
+    return false;
+  }
+
+  { // write attributes in a single file.
+    vsl_b_ofstream f(dir + "/attributes.vsl");
+    vsl_b_write(f, attr_);
+  }
+
+  // Optionally tar-gzip the directory. Perhaps using an external shell script.
+
+  return true;
+}
+
+bool bmcsd_curve_3d_sketch_e::
+read_dir_format(std::string dir)
+{
+  // Optionally tar-gzip the directory. Perhaps using an external shell script.
+
+  short ver = 0;
+  { // version number 
+    std::ifstream f((dir + "/version").c_str());
+    f >> ver;
+  }
+
+  switch (ver) {
+    case 1: {
+              std::cerr << "Obsolete curve sketch version number 1: Run the script mcs_fix_numbering with the right number of digits (ceil(log(ncurves)/log(10))+1, bump up the version to 2, then call this script." 
+                << std::endl;
+              return false;
+    } break;
+    case 2: {
+
+    unsigned ncurves;
+    std::cout << "read_dir: Reading num_curves\n";
+    { // number 
+      std::ifstream f((dir + "/num_curves").c_str());
+      f >> ncurves;
+      crv3d_.resize(ncurves);
+    }
+
+    std::cout << "read_dir: Reading 3d curves\n";
+    {
+      bool retval = myreadv(dir + std::string("/crvs/rec"), ".dat", crv3d_);
+      if(!retval) {
+        std::cerr << "Error while trying to read 3D curves from dir " << dir << std::endl;
+        return false;
+      }
+    }
+
+    std::cout << "read_dir: Reading attributes\n";
+    {
+    vsl_b_ifstream f((dir + "/attributes.vsl").c_str());
+    vsl_b_read(f, attr_);
+#ifndef NDEBUG
+    std::cout << "attr_.size(): "<< attr_.size() << " ncurves: " << ncurves << std::endl;
+#endif
+    assert(attr_.size() == ncurves);
+    }
+
+    } break;
+    default:
+      std::cerr << "I/O ERROR: bmcsd_curve_3d_sketch_e::read_dir_format\n"
+           << "           Unknown version number "<< ver << '\n';
+  }
+
+  return true;
+}
---
+++ vxd/contrib/brld/bmvgd/bmcsd/bmcsd_curve_3d_sketch_e.h
@@ -0,0 +1,116 @@
+// This is bmcsd_curve_3d_sketch_e.h
+#ifndef bmcsd_curve_3d_sketch_e_h
+#define bmcsd_curve_3d_sketch_e_h
+//:
+//\file
+//\brief File to hold 3D curves and attributes
+//\author Ricardo Fabbri (rfabbri), Brown University  (@gmail.com)
+//\date 11/11/09 18:24:24 EST
+//
+
+#include <bdifd/bdifd_frenet.h>
+#include <bmcsd/bmcsd_view_set.h>
+#include <bmcsd/bmcsd_curve_3d_attributes_e.h>
+#include <bmcsd/bmcsd_util.h>
+
+// This class holds 3D curves together with attributes such as pointers to which
+// views were used to reconstruct them, etc. It is used as a basic
+// representation for communicating between multiview curve stereo and multiview
+// curve photogrammetry
+class bmcsd_curve_3d_sketch_e {
+public:
+
+  bmcsd_curve_3d_sketch_e() {}
+
+  //: You should be using this always with attributes, otherwise just use the
+  // curve itself.
+  bmcsd_curve_3d_sketch_e(
+      const std::vector< bdifd_1st_order_curve_3d > &crv3d,
+      const std::vector< bmcsd_curve_3d_attributes > &attr)
+  : 
+    crv3d_(crv3d),
+    attr_(attr)
+  { 
+    assert(attr_.size() == crv3d.size());
+  }
+
+
+  void set(const std::vector< bdifd_1st_order_curve_3d > &crv3d,
+      const std::vector< bmcsd_curve_3d_attributes > &attr)
+  {
+    crv3d_ = crv3d;
+    attr_ = attr;
+    assert(attr_.size() == crv3d.size());
+  }
+
+  //: Quick n' dirty: write in a directory structure format. The directory will
+  // contain a file for each 3D curve, and for each 3D curve there will be a
+  // corresponding entry on an attributes.vsl file with the remaining attributes
+  // in text format.
+  bool write_dir_format(std::string dirname) const;
+  bool read_dir_format(std::string dirname);
+  short dir_format_version() const { return 2;}
+  unsigned num_curves() const { return crv3d_.size(); }
+  unsigned total_num_points() const {
+    unsigned npts=0;
+    for (unsigned c=0; c < num_curves(); ++c)
+      npts += curves_3d()[c].size();
+    return npts;
+  }
+
+  //: Getters and setters. Setters should assert for consistency.
+  const std::vector< bdifd_1st_order_curve_3d > & curves_3d() const
+  { return crv3d_; }
+  const std::vector< bmcsd_curve_3d_attributes_e > &attributes() const
+  { return attr_; }
+
+  typedef std::vector<vgl_point_3d<double> > curve_points;
+
+  void
+  get_curves_as_vgl_points(std::vector< curve_points > *pts3d_ptr) const
+  {
+    std::vector< curve_points > &pts3d = *pts3d_ptr;
+    pts3d.resize(crv3d_.size());
+
+    for (unsigned c=0; c < num_curves(); ++c) {
+      pts3d[c].resize(crv3d_[c].size());
+      for (unsigned p=0; p < crv3d_[c].size(); ++p) {
+        const bmcsd_vector_3d &pt = crv3d_[c][p].Gama;
+        pts3d[c][p] = vgl_point_3d<double>(pt[0], pt[1], pt[2]);
+      }
+    }
+  }
+
+  bool operator==(const bmcsd_curve_3d_sketch_e &o) const { 
+    return crv3d_ == o.crv3d_ && attr_ == o.attr_;
+  }
+
+  //: prune
+  void prune_by_total_support(double tau_support)
+  {
+    std::vector< bdifd_1st_order_curve_3d > crv3d_new;
+    std::vector< bmcsd_curve_3d_attributes_e > attr_new;
+
+    crv3d_new.reserve(num_curves());
+    attr_new.reserve(num_curves());
+
+    for (unsigned i=0; i < num_curves(); ++i) {
+      if (attributes()[i].total_support_ >= tau_support) {
+        crv3d_new.push_back(curves_3d()[i]);
+        attr_new.push_back(attributes()[i]);
+      }
+    }
+
+    crv3d_ = crv3d_new;
+    attr_ = attr_new;
+  }
+
+  unsigned totalMatchCount_;
+  unsigned totalReconCount_;
+
+//private:
+  std::vector< bdifd_1st_order_curve_3d > crv3d_;
+  std::vector< bmcsd_curve_3d_attributes_e > attr_;
+};
+
+#endif // bmcsd_curve_3d_sketch_e_h
