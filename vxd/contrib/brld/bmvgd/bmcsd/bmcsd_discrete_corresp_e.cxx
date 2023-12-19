e03f87a1af1de4387777ff3c2fcdbb3ae5079a1a
Ricardo Fabbri <rfabbri@gmail.com>
2023-12-19 19:34:36 -0300

    [3DCD] enchanced discrete corresp structure from anil

---
+++ vxd/contrib/brld/bmvgd/bmcsd/bmcsd_discrete_corresp_e.cxx
@@ -0,0 +1,560 @@
+#include "bmcsd_discrete_corresp_e.h"
+#include <vsl/vsl_vector_io.h>
+#include <vsl/vsl_list_io.h>
+#include <algorithm>
+#include <set>
+#include <bmcsd/bmcsd_util.h>
+
+bool  bmcsd_discrete_corresp_e::
+add_unique(const bmcsd_attributed_object_e &e, unsigned i, 
+    std::list<bmcsd_attributed_object_e>::iterator *itr
+    )
+{
+  std::list<bmcsd_attributed_object_e>::iterator 
+    result = find(corresp_[i].begin(), corresp_[i].end(), e); 
+
+  if (result != corresp_[i].end()) {
+    *itr = result;
+    return false;
+  }
+
+  if (i >= corresp_.size()) {
+    std::cout << "bmcsd_discrete_corresp_e:: resizing list\n";
+    corresp_.resize(i+1);
+  }
+
+  corresp_[i].push_back(e);
+  *itr = --corresp_[i].end();
+
+  return true;
+}
+
+
+
+std::ostream&  operator<<(std::ostream& s, const bmcsd_attributed_object_e &c)
+{
+  c.print_summary(s);
+  return s;
+}
+
+std::ostream&  operator<<(std::ostream& s, const bmcsd_discrete_corresp_e &c)
+{
+  s << "===========  bmcsd_discrete_corresp_e ===========\n";
+  s << "# of Domain objects = num_objs0 + 1 = " << c.corresp_.size() << std::endl;
+
+  unsigned  max_domain_elts=20, count_nonzero=0;
+  unsigned  i;
+  unsigned  total_n_corresp=0;
+  for (i=0; i < c.corresp_.size(); ++i) {
+    if (i < max_domain_elts) {
+      s << "obj_id: " << i << " #corresp: " << c.corresp_[i].size() << std::endl;
+
+      std::list< bmcsd_attributed_object_e >::const_iterator  itr;
+      const unsigned  max_elts=3; 
+      unsigned  nn=1;
+      for (itr = c.corresp_[i].begin(); itr != c.corresp_[i].end(); ++itr, ++nn) {
+        if (nn > max_elts)
+          break;
+        s << "         "; 
+        itr->print_summary(s);
+        s << std::endl;
+      }
+    }
+    if (!c.corresp_[i].empty())
+      ++count_nonzero;
+    total_n_corresp += c.corresp_[i].size();
+  }
+
+
+  unsigned n_finite=0;
+  for (i=0; i < c.corresp_.size(); ++i) {
+      std::list< bmcsd_attributed_object_e >::const_iterator  itr;
+      for (itr = c.corresp_[i].begin(); itr != c.corresp_[i].end(); ++itr) {
+        if (vnl_math_isfinite(itr->cost_))
+          n_finite++;
+      }
+  }
+
+  if (i >= max_domain_elts)
+    std::cout << "...\n";
+
+  s << "Total domain objects with some correspondents: " << count_nonzero << std::endl;
+  s << "Total number of corresps: " << total_n_corresp << std::endl;
+  s << "Avg. number of corresps per domain object: " << (float)total_n_corresp/(float)c.corresp_.size() << std::endl;
+  s << "Number of corresps with finite cost: " << n_finite << std::endl;
+  s << "============================================\n";
+
+  return s;
+}
+
+
+// I/O ------------------------------------------------------------------
+
+
+//: Binary save self to stream.
+void 
+bmcsd_attributed_object_e::
+b_write(vsl_b_ostream &os) const
+{
+  vsl_b_write(os, version());
+  vsl_b_write(os,obj_);
+  vsl_b_write(os,null_);
+  vsl_b_write(os,cost_);
+}
+
+//: Binary load self from stream.
+void 
+bmcsd_attributed_object_e::
+b_read(vsl_b_istream &is)
+{
+  if (!is) return;
+
+  short ver;
+  vsl_b_read(is, ver);
+  switch (ver)
+  {
+    case 1:
+      vsl_b_read(is, obj_);
+      vsl_b_read(is, null_);
+      vsl_b_read(is, cost_);
+
+    break;
+
+    default:
+        std::cerr << "I/O ERROR: bmcsd_attributed_object_e::b_read(vsl_b_istream&)\n"
+             << "           Unknown version number "<< ver << '\n';
+    is.is().clear(std::ios::badbit); // Set an unrecoverable IO error on stream
+    return;
+  }
+}
+
+//: Return IO version number;
+short 
+bmcsd_attributed_object_e::
+version() const
+{
+  return 1;
+}
+
+//: Print an ascii summary to the stream
+void 
+bmcsd_attributed_object_e::
+print_summary(std::ostream &os) const
+{
+  os << "[" << is_a() << ": " << 
+     "is_null: " << null_ << "  object_id: " << obj_ << "  cost: " << cost_
+     << "]";
+}
+
+//: Return a platform independent string identifying the class
+std::string 
+bmcsd_attributed_object_e::
+is_a() const 
+{return "bmcsd_attributed_object_e";}
+
+
+//: Binary save self to stream.
+void 
+bmcsd_discrete_corresp_e::
+b_write(vsl_b_ostream &os) const
+{
+  vsl_b_write(os, version());
+  vsl_b_write(os, cksum_);
+  unsigned n = n0();
+  vsl_b_write(os, n);
+  n = n1();
+  vsl_b_write(os, n);
+  for (unsigned i=0; i < corresp_.size(); ++i) {
+    vsl_b_write<bmcsd_attributed_object_e>(os, corresp_[i]);
+  }
+}
+
+//: Binary load self from stream.
+void 
+bmcsd_discrete_corresp_e::
+b_read(vsl_b_istream &is)
+{
+  if (!is) return;
+
+  short ver;
+  vsl_b_read(is, ver);
+  switch (ver)
+  {
+    case 1: {
+      std::cerr << "Warning: this is an old correspondence file...\n"
+       << "...it is incompatible with some newer tools\n";
+      unsigned size;
+      vsl_b_read(is, size);
+      corresp_.resize(size);
+      for (unsigned i=0; i < size; ++i)
+        vsl_b_read<bmcsd_attributed_object_e>(is, corresp_[i]);
+    } break;
+
+    case 2: {
+      unsigned n0, n1;
+      vsl_b_read(is, n0);
+      vsl_b_read(is, n1);
+      assert (n0 != 0 && n1 != 0);
+      set_size(n0-1, n1-1);
+      for (unsigned i=0; i < n0; ++i)
+        vsl_b_read<bmcsd_attributed_object_e>(is, corresp_[i]);
+    } break;
+    case 3: {
+      unsigned long cksum;
+      vsl_b_read(is, cksum);
+      set_checksum(cksum);
+      unsigned n0, n1;
+      vsl_b_read(is, n0);
+      vsl_b_read(is, n1);
+      assert (n0 != 0 && n1 != 0);
+      set_size(n0-1, n1-1);
+      for (unsigned i=0; i < n0; ++i)
+        vsl_b_read<bmcsd_attributed_object_e>(is, corresp_[i]);
+    } break;
+
+    default:
+        std::cerr << "I/O ERROR: bmcsd_discrete_corresp_e::b_read(vsl_b_istream&)\n"
+             << "           Unknown version number "<< ver << '\n';
+    is.is().clear(std::ios::badbit); // Set an unrecoverable IO error on stream
+    return;
+  }
+}
+
+//: Return IO version number;
+short 
+bmcsd_discrete_corresp_e::
+version() const
+{
+  return 4; // bumped to 4 for enhanced curve sketch, even though it is the same
+            // as 3 for now
+}
+
+//: Print an ascii summary to the stream
+void 
+bmcsd_discrete_corresp_e::
+print_summary(std::ostream &os) const
+{
+  os << *this;
+}
+
+//: Return a platform independent string identifying the class
+std::string 
+bmcsd_discrete_corresp_e::
+is_a() const 
+{return "bmcsd_discrete_corresp_e";}
+
+//: remove every element whose cost is above threshold
+//\todo test
+void bmcsd_discrete_corresp_e::
+threshold_by_cost(double max_cost)
+{
+  for (unsigned i=0; i < corresp_.size(); ++i) {
+    std::list< bmcsd_attributed_object_e >::iterator  itr;
+    for (itr = corresp_[i].begin(); itr != corresp_[i].end();) {
+      if (itr->cost_ > max_cost)
+        itr = corresp_[i].erase(itr);
+      else
+        ++itr;
+    }
+  }
+}
+
+//: remove every element whose cost is below or equal to threshold
+void bmcsd_discrete_corresp_e::
+threshold_by_cost_lteq(double min_cost)
+{
+  for (unsigned i=0; i < corresp_.size(); ++i) {
+    std::list< bmcsd_attributed_object_e >::iterator  itr;
+    for (itr = corresp_[i].begin(); itr != corresp_[i].end();) {
+      if (itr->cost_ <= min_cost)
+        itr = corresp_[i].erase(itr);
+      else
+        ++itr;
+    }
+  }
+}
+
+void bmcsd_discrete_corresp_e::
+sort()
+{
+  for (unsigned i=0; i < corresp_.size(); ++i) {
+    corresp_[i].sort();
+  }
+}
+
+//: \param[in] gt : ground-truth correspondences
+void bmcsd_discrete_corresp_e::
+compare_and_print( const bmcsd_discrete_corresp_e *gt) const
+{
+  assert(this->size() == gt->size());
+
+  std::cout << "\n========== CORRESPONDENCE STATISTICS ==========\n"; 
+
+
+  unsigned n_w_gt;
+  number_of_pts1_with_gt_among_any_candidates(n_w_gt,gt);
+
+  std::cout << "# of objects in image 1 having ground-truth among any candidates:\n\t" 
+    << 100.0*(float)n_w_gt/(float)(size()) << "% ("  << n_w_gt << " out of " << size() << ")." << std::endl;
+
+  unsigned n5=0, 
+           n_top_50=0, 
+           n_gt=0, 
+           total_nmatches_gt=0;//:< total number of candidates with non-empty ground-truth 
+  for (unsigned i=0; i < this->corresp_.size(); ++i) {
+    // for each top 5 match:
+
+    if (gt->corresp_[i].empty()) {
+      continue;
+    } else {
+      bool found_gt_in_top_5=false,
+           found_gt_in_top_half=false, has_finite=false;
+      unsigned ncorr_atual = 0;
+      double corresp_i_size = (double)(corresp_[i].size());
+      std::list< bmcsd_attributed_object_e >::const_iterator  itr;
+      for (itr = corresp_[i].begin(); itr != corresp_[i].end(); ++itr) {
+        if (!vnl_math_isfinite(itr->cost_))
+          continue;
+        else
+          has_finite = true;
+
+        ++total_nmatches_gt;
+        ++ncorr_atual;
+
+
+        std::list<bmcsd_attributed_object_e>::const_iterator 
+          result = find_if(gt->corresp_[i].begin(), gt->corresp_[i].end(), bmcsd_attributed_object_eq(itr->obj_)); 
+
+        if (result != gt->corresp_[i].end()) {
+          if (vnl_math_isfinite(result->cost_)) {
+            // found
+            if (ncorr_atual <= 5)
+              found_gt_in_top_5 = true;
+            if (ncorr_atual <= ceil(corresp_i_size/2.0))
+              found_gt_in_top_half = true;
+          } else {
+            std::cout << "Warning: non-finite cost in ground-truth corresp\n";
+          }
+        }
+      }
+      if (has_finite)
+        n_gt++;
+      if (found_gt_in_top_half)
+        n_top_50++;
+      if (found_gt_in_top_5)
+        n5++;
+    }
+  }
+
+  std::cout << "# objects in image 1 having correct candidates ranked among top 50% matches:\n\t" 
+    << 100.0*(float)n_top_50/(float)n_gt << "% ("  << n_top_50 << " out of " << n_gt  << ")." << std::endl;
+
+  std::cout << "# objects in image 1 having correct candidates in top 5 matches:\n\t" 
+    << 100.0*(float)n5/(float)n_gt << "% ("  << n5 << " out of " << n_gt  << ")." << std::endl;
+
+  std::cout << "Top 5 matches covers " << (float)(100*std::min(5*n_gt,total_nmatches_gt))/(float)total_nmatches_gt << "\% of the total candidate matches (" << std::min(5*n_gt,total_nmatches_gt) << " out of " << total_nmatches_gt << ").\n";
+
+
+  unsigned nh_higher, nh_valid;
+  percentage_of_matches_above_truth(nh_higher,nh_valid,gt);
+
+  std::cout << "# Matches with cost higher than correct match:\n\t" 
+    << 100.0*(float)nh_higher/(float)nh_valid<< "% ("  << nh_higher << " out of " << nh_valid << ")." << std::endl;
+
+  // - percentage of objects in image 1 having correct candidates in top 5 first matches
+
+  // - percentage of objects in image 1 having correct candidates in top 10 first
+  // matches
+  
+  // - any other statistics?
+
+
+}
+
+//: Computes the percentage of all *wrong* epipolar candidates having energy above the
+// ground-truth.
+//
+// More precisely, we want "How many of the *wrong* correspondences passing a
+// test have cost greater than the true correspondences? By passing a test it
+// means it has finite cost. 
+//
+// TODO: test further
+void bmcsd_discrete_corresp_e::
+percentage_of_matches_above_truth(unsigned &n, unsigned &n_valid, const bmcsd_discrete_corresp_e *gt) const
+{
+//for each object p1 in image 1
+
+  n = n_valid = 0;
+  for (unsigned p1_idx=0; p1_idx < this->corresp_.size(); ++p1_idx) {
+    std::list<bmcsd_attributed_object_e>::const_iterator  result;
+    result = this->find_right_corresp_mincost(p1_idx,gt);
+    if (result != this->corresp_[p1_idx].end()) { // found
+
+      //  For each wrong correspondence
+      //    If its cost is greater than result->cost_, n++;
+
+      std::list< bmcsd_attributed_object_e >::const_iterator  itr = corresp_[p1_idx].begin();
+      for (; itr != corresp_[p1_idx].end(); ++itr) {
+        if (vnl_math_isfinite(itr->cost_) && itr->obj_ != result->obj_) {
+          ++n_valid;
+
+          if (itr->cost_ > result->cost_)
+            ++n;
+        }
+      }
+    }
+  }
+}
+
+//: \remarks if there are more than one possible ground-truths for a given
+// p1, then it counts if at least one of them is within the list of candidates.
+//
+// If the list of candidates is empty, it counts iff corresp gt list is empty. 
+//
+// If the list of candidates is non-empty, and gt is empty, then we do not
+// count.
+//
+void bmcsd_discrete_corresp_e::
+number_of_pts1_with_gt_among_any_candidates(unsigned &n_w_gt, const bmcsd_discrete_corresp_e *gt) const
+{
+  n_w_gt = 0;
+  for (unsigned p1_idx=0; p1_idx < this->size(); ++p1_idx) {
+    std::list< bmcsd_attributed_object_e >::const_iterator  itr;
+
+    // Special cases
+    if (corresp_[p1_idx].empty()) {
+      if (gt->corresp_[p1_idx].empty())
+        ++n_w_gt;
+    } else {
+      if (!gt->corresp_[p1_idx].empty()) {
+        bool found=false;
+        for (itr = corresp_[p1_idx].begin(); itr != corresp_[p1_idx].end() && !found; ++itr) {
+          std::list<bmcsd_attributed_object_e>::const_iterator 
+            result = find_if(gt->corresp_[p1_idx].begin(), gt->corresp_[p1_idx].end(), bmcsd_attributed_object_eq(itr->obj_)); 
+
+          if (result != gt->corresp_[p1_idx].end()) {
+            found = true;
+            ++n_w_gt;
+          }
+        }
+      }
+    }
+  }
+}
+
+//: returns pointer to the right match (p2) having the lowest cost in gt
+// returned iterator is equal to corresp_[p1_idx].last() in case nothing was
+// found.
+std::list<bmcsd_attributed_object_e>::const_iterator bmcsd_discrete_corresp_e::
+find_right_corresp_mincost(unsigned p1_idx, const bmcsd_discrete_corresp_e *gt) const
+{
+  // run through containing all the candidates of p1_idx
+  // also appearing in the ground-truth.
+
+  unsigned i = p1_idx;
+  std::list<bmcsd_attributed_object_e>::const_iterator min_cost_itr = corresp_[i].end();
+  double min_cost = std::numeric_limits<double>::infinity();
+
+  if (gt->corresp_[i].empty())
+    return min_cost_itr;
+
+  std::list< bmcsd_attributed_object_e >::const_iterator  itr;
+  for (itr = corresp_[i].begin(); itr != corresp_[i].end(); ++itr) {
+    if (!vnl_math_isfinite(itr->cost_))
+      continue;
+
+    std::list<bmcsd_attributed_object_e>::const_iterator 
+      result = find_if(gt->corresp_[i].begin(), gt->corresp_[i].end(), bmcsd_attributed_object_eq(itr->obj_)); 
+
+    if (result != gt->corresp_[i].end()) {
+      if (vnl_math_isfinite(result->cost_)) {
+        // found
+        if (itr->cost_ < min_cost) {
+          min_cost = itr->cost_;
+          min_cost_itr = itr;
+        }
+      } else {
+        std::cout << "Warning: non-finite cost in ground-truth corresp\n";
+      }
+    }
+  }
+
+  return min_cost_itr;
+}
+
+//: 
+// purpose: to determine if there is a ground-truth correspondence among top 5
+// putative correspondences of a given object in image 1.
+//
+// \param[in] p1_idx: index of a object in image 1
+//
+// \return: true or false
+//
+// \remarks: in the special case of an empty gt for p1_idx, return true
+//
+bool bmcsd_discrete_corresp_e::
+is_gt_among_top5(unsigned p1_idx, const bmcsd_discrete_corresp_e *gt) const
+{
+
+  unsigned i = p1_idx;
+  std::list<bmcsd_attributed_object_e>::const_iterator min_cost_itr = corresp_[i].end();
+
+  if (gt->corresp_[i].empty())
+    return true;
+
+  unsigned ncorr_atual = 0;
+  std::list< bmcsd_attributed_object_e >::const_iterator  itr;
+  for (itr = corresp_[i].begin(); itr != corresp_[i].end() && ncorr_atual < 5; ++itr, ++ncorr_atual) {
+    if (!vnl_math_isfinite(itr->cost_))
+      continue;
+
+    std::list<bmcsd_attributed_object_e>::const_iterator 
+      result = find_if(gt->corresp_[i].begin(), gt->corresp_[i].end(), bmcsd_attributed_object_eq(itr->obj_)); 
+
+    if (result != gt->corresp_[i].end()) {
+      if (vnl_math_isfinite(result->cost_)) { 
+        return true;
+      } else {
+        std::cout << "Warning: non-finite cost in ground-truth corresp\n";
+      }
+    }
+  }
+
+  return false;
+}
+
+void bmcsd_discrete_corresp_e::
+keep_only_unambiguous_max(double ratio, double lonely_threshold)
+{
+  //  std::cout << "Using lowe's criterion with ratio " << ratio << std::endl;
+  sort();
+  for (unsigned i=0; i < n0(); ++i) {
+    unsigned sz = corresp_[i].size();
+    if ( sz == 0)
+      continue;
+
+    bmcsd_attributed_object_e attr = corresp_[i].back();
+
+    if (sz == 1) {
+      corresp_[i].clear();
+      // Only accept a lonely dog if it is way beyond threshold
+      if (attr.cost() > ratio*lonely_threshold)
+        corresp_[i].push_back(attr);
+      continue;
+    }
+
+    // sz >= 2
+
+    bmcsd_attributed_object_e 
+      attr_second = *(--(--corresp_[i].end()));
+
+    corresp_[i].clear();
+
+    //    std::cout << "second cost: " << attr_second.cost()
+    //      << " first cost: "<< attr.cost() << std::endl;
+    if (ratio*attr_second.cost() < attr.cost()) {
+    //      std::cout << "Inserting unambiguous\n";
+      corresp_[i].push_back(attr);
+    }
+  }
+}
---
+++ vxd/contrib/brld/bmvgd/bmcsd/bmcsd_discrete_corresp_e.h
@@ -0,0 +1,316 @@
+// This is bmcsd_discrete_corresp_e.h
+#ifndef bmcsd_discrete_corresp_e_h
+#define bmcsd_discrete_corresp_e_h
+//:
+//\file
+//\brief Data structure representing correspondence between two sets of objects
+//\author Ricardo Fabbri (rfabbri), Brown University  (@gmail.com)
+//\date 04/25/2005 06:41:03 PM EDT
+// 
+// Modifications:
+// Wed Sep 30 02:12:23 EDT 2009   Ricardo Fabbri  Renamed things: s/point/object/g
+//
+
+#include <cassert>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <vsl/vsl_binary_io.h>
+#include <bmcsd/bmcsd_discrete_corresp.h> // for _e version we use some of the original
+#include <vcl_set.h>
+
+
+// mw_match_attribute is from original (without _e)
+
+//: This is to be used within the bmcsd_discrete_corresp_e class;
+struct bmcsd_attributed_object_e : public bmcsd_attributed_object {
+public:
+  bmcsd_attributed_object_e() { }
+
+  bmcsd_attributed_object_e(unsigned obj_idx, bool isnull=false, double cost=0)
+   :
+    bmcsd_match_attribute(isnull, cost),
+    obj_(obj_idx)
+  {
+  }
+  bmcsd_attributed_object(unsigned obj_idx, unsigned container_idx, bool isnull=false, double cost=0)
+   :
+    mw_match_attribute(isnull, cost),
+    obj_(obj_idx),
+    container_id_(container_idx)
+  {
+  }
+
+  bmcsd_attributed_object(unsigned obj_idx, std::vector<std::set<int> > supportStructures, 
+                     std::vector<unsigned> inliers, bool isnull=false, double cost=0)
+   :
+    mw_match_attribute(isnull, cost),
+    obj_(obj_idx),
+    supportingStructures_(supportStructures),
+    inliers_(inliers)
+  {
+  }
+
+  bmcsd_attributed_object(unsigned obj_idx, unsigned container_idx, std::vector<std::set<int> > supportStructures, 
+                     std::vector<unsigned> inliers, bool isnull=false, double cost=0)
+   :
+    mw_match_attribute(isnull, cost),
+    obj_(obj_idx),
+    supportingStructures_(supportStructures),
+    inliers_(inliers),
+    container_id_(container_idx)
+  {
+  }
+
+  bmcsd_attributed_object(unsigned obj_idx, unsigned container_idx, std::vector<std::set<int> > supportStructures, 
+		       std::vector<unsigned> inliers, std::vector<unsigned> supportCount, 
+		       bool isnull=false, double cost=0, std::vector<std::vector<int> > indexChain=std::vector<std::vector<int> >())
+   :
+    mw_match_attribute(isnull, cost),
+    obj_(obj_idx),
+    supportingStructures_(supportStructures),
+    inliers_(inliers),
+    container_id_(container_idx),
+    support_count_(supportCount),
+    index_chain_(indexChain)
+  {
+  }
+
+  ~bmcsd_attributed_object_e() {}
+  
+
+
+  // I/O ------------------------------------------------------------------
+
+  //: Binary save self to stream.
+  void b_write(vsl_b_ostream &os) const;
+
+  //: Binary load self from stream.
+  void b_read(vsl_b_istream &is);
+
+  //: Return IO version number;
+  short version() const;
+  
+  //: Print an ascii summary to the stream
+  void print_summary(std::ostream &os) const;
+
+  friend std::ostream&  operator<<(std::ostream& s, const bmcsd_attributed_object_e &c);
+
+  std::string is_a() const;
+
+public:
+   //Anil: The ID for the container of obj_ (in multiview stereo, this is view ID)
+   unsigned container_id_;
+
+   //Anil: A vector of supporting structure list, implemented with multiview edge support in mind
+   //TODO: Make abstract, maybe move to a subclass
+   std::vector<std::set<int> > supportingStructures_;
+
+   //Anil: A vector of supporting structure count, implemented with multiview edge support in mind
+   std::vector<unsigned> support_count_;
+
+   //Anil: A 2D vector of supporting structure IDs (per view, per sample)
+   std::vector<std::vector<int> > index_chain_;
+
+   //Anil: A vector of inlier IDs, implemented with multiview stereo in mind
+   std::vector<unsigned> inliers_;
+};
+
+class bmcsd_attributed_object_eq : public std::unary_function<bmcsd_attributed_object_eq, bool> {
+  unsigned p_;
+public:
+  explicit bmcsd_attributed_object_eq(const unsigned pp) : p_(pp) { }; 
+  bool operator() (const bmcsd_attributed_object_e &e) const { return e.obj_ == p_; }
+};
+
+//: Binary save vnl_my_class to stream.
+inline void vsl_b_write(vsl_b_ostream &os, const bmcsd_attributed_object_e & v)
+{
+  v.b_write(os);
+}
+
+//: Binary load vnl_my_class from stream.
+inline void vsl_b_read(vsl_b_istream &is, bmcsd_attributed_object_e & v)
+{
+  v.b_read(is);
+}
+
+inline void vsl_print_summary(std::ostream &os, const bmcsd_attributed_object_e &p)
+{
+  p.print_summary(os);
+}
+
+
+//: We support many-to-one, one-to-many, and even zero-to-many and many-to-zero
+// Access is directly on datastructure, since its kinda low-level
+//
+// The object sets are indexed in the same way as in the initialization object
+// vectors.
+//
+// The representation for an object is an id (unsigned int). Therefore, objects
+// can be anything having an id. Each object can be a single point, or a SIFT
+// point, or a curve fragment. Anything goes.
+//
+// Enhnanced _e version is same as original, but using attributed_object_e
+struct bmcsd_discrete_corresp_e {
+public:
+  typedef std::list< bmcsd_attributed_object_e > one_corresp_list;
+  typedef std::list< bmcsd_attributed_object_e >::iterator one_corresp_list_iter;
+  typedef std::list< bmcsd_attributed_object_e >::const_iterator one_corresp_list_const_iter;
+  typedef std::vector < std::list< bmcsd_attributed_object_e > > corresp_data;
+
+  bmcsd_discrete_corresp_e(unsigned num_objs0, unsigned num_objs1) 
+     :
+     corresp_(num_objs0+1),
+     num_counterdomain_(num_objs1+1),
+     cksum_(num_objs0+2*num_objs1) // arbitrary hash.
+  { }
+  bmcsd_discrete_corresp_e() {}
+  ~bmcsd_discrete_corresp_e() {}
+
+  void set_size(unsigned num_objs0, unsigned num_objs1) 
+  { corresp_.resize(num_objs0+1); num_counterdomain_ = num_objs1+1; }
+
+  //: Set a checksum for the objects in the domain and counter-domain. This is
+  // to ensure this correspondence is for the objects it was intended for.
+  void set_checksum(unsigned long long c) { cksum_ = c;}
+  unsigned long checksum() const {return cksum_; }
+
+  //: adds object, testing if is unique, and also resize corresp. list if needed
+  bool add_unique(const bmcsd_attributed_object_e &e, unsigned i, 
+      std::list<bmcsd_attributed_object_e>::iterator *itr);
+
+  void threshold_by_cost(double cost);
+  void threshold_by_cost_lteq(double cost);
+  void sort();
+
+  //: \param[in] maxmin : if true, keep only max cost. if false, keep only min
+  // cost.
+  void keep_only_extreme_cost(bool keep_max)
+  {
+    sort();
+    for (unsigned i=0; i < n0(); ++i) {
+      if (corresp_[i].empty())
+        continue;
+      bmcsd_attributed_object_e attr;
+      if (keep_max)
+        attr = corresp_[i].back();
+      else
+        attr = corresp_[i].front();
+      corresp_[i].clear();
+      corresp_[i].push_back(attr);
+    }
+  }
+
+  //: Use lowe-style disambiguation, i.e.,
+  // keep only the match having max #votes,
+  // if this #votes is better than the ratio*#votes of second best.
+  //
+  // If there is no second best to compare, demand that the match be way beyond
+  // lonely_threshold.
+  //
+  // Extremes behavior is as follows:
+  //
+  // With ratio == 0, the behavior is that of keeping the max cost.
+  //
+  // With ratio != 0 and: 
+  //    - lonely_threshold == infinity, the behavior is to never include loners
+  //    - lonely_threshold == 0, the behavior is to always include loners
+  //
+  // With ratio == infinity and lonely_threshold != infinity, the behavior is to
+  // never include correspondences having a second best, no matter how bad the
+  // second best is.
+  //
+  // This assumes there are no correspondences with 0 or Inf cost.
+  void keep_only_unambiguous_max(double ratio=1.5, double lonely_threshold = 5.0);
+
+  //: returns the total number of correspondences.
+  unsigned num_corresps() const
+  {
+    unsigned sum = 0;
+    for (unsigned i=0; i < n0(); ++i) {
+      sum += corresp_[i].size();
+    }
+    return sum;
+  }
+  const one_corresp_list & operator[](unsigned i) const { assert (i < n0()); return corresp_[i]; }
+  one_corresp_list & operator[](unsigned i) { assert (i < n0()); return corresp_[i]; }
+
+  // todo: add as heap / sorted insert
+
+  // Functions to be moved to algo ----------------------------------------
+
+  std::list<bmcsd_attributed_object_e>::const_iterator 
+  find_right_corresp_mincost(unsigned p1_idx, const bmcsd_discrete_corresp_e *gt) const;
+
+  void 
+  percentage_of_matches_above_truth(unsigned &n, unsigned &n_valid, const bmcsd_discrete_corresp_e *gt) const;
+
+  void
+  number_of_pts1_with_gt_among_any_candidates(unsigned &n_w_gt, const bmcsd_discrete_corresp_e *gt) const;
+
+  bool is_gt_among_top5(unsigned p1_idx, const bmcsd_discrete_corresp_e *gt) const;
+
+  //: \todo functional access
+
+  friend std::ostream&  operator<<(std::ostream& s, const bmcsd_discrete_corresp_e &c);
+
+  //: Equality test
+  inline bool operator==(bmcsd_discrete_corresp_e const &that) const
+  { return corresp_ == that.corresp_; }
+
+  // I/O ------------------------------------------------------------------
+
+  //: Binary save self to stream.
+  void b_write(vsl_b_ostream &os) const;
+
+  //: Binary load self from stream.
+  void b_read(vsl_b_istream &is);
+
+  //: Return IO version number;
+  short version() const;
+
+  //: Print an ascii summary to the stream
+  void print_summary(std::ostream &os) const;
+
+  //: Return a platform independent string identifying the class
+  std::string is_a() const;
+
+  //: # objects in view 1; returns (unsigned) -1  in case nothing was initialized yet
+  unsigned n_objects_view_0() const {return corresp_.size() - 1; }
+  unsigned n_objects_view_1() const {return n1() - 1; }
+  unsigned n0() const { return corresp_.size(); }
+  unsigned n1() const { return num_counterdomain_;}
+
+  unsigned size() const {return n_objects_view_0(); }
+  bool is_empty() const {return corresp_.size() == 0; }
+
+  // returns the number of objects (in image 1) having no correspondence.
+  // If the dummy object has no correspondence, it also counts.
+  unsigned count_empty() const { 
+    return std::count_if(corresp_.begin(), corresp_.end(), 
+                        std::mem_fun_ref(&one_corresp_list::empty )); 
+  }
+
+  void compare_and_print( const bmcsd_discrete_corresp_e *gt) const;
+
+public:
+   corresp_data corresp_;
+private:
+   unsigned num_counterdomain_;
+   unsigned long cksum_;
+};
+
+//: Binary save vnl_my_class to stream.
+inline void vsl_b_write(vsl_b_ostream &os, const bmcsd_discrete_corresp_e & v)
+{
+  v.b_write(os);
+}
+
+//: Binary load vnl_my_class from stream.
+inline void vsl_b_read(vsl_b_istream &is, bmcsd_discrete_corresp_e & v)
+{
+  v.b_read(is);
+}
+
+#endif // bmcsd_discrete_corresp_e_h
