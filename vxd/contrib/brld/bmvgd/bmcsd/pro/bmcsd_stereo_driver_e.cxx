8aaf3a3656f9ece52a7effe914e2454641babeeb
Ricardo Fabbri <rfabbri@gmail.com>
2024-01-18 09:15:47 -0300

    [3DCD] extended stereo_driver hierarchy

---
+++ vxd/contrib/brld/bmvgd/bmcsd/pro/bmcsd_stereo_driver_e.cxx
@@ -0,0 +1,47 @@
+#include "bmcsd_stereo_driver_e.h"
+#include <bmcsd/pro/bmcsd_fragment_tangents_filter.h>
+#include <bmcsd/pro/bmcsd_load_curvelet_source.h>
+#include <bmcsd/pro/bmcsd_stereo_filter_e.h>
+#include <algorithm>
+
+bool bmcsd_concurrent_stereo_driver_e::
+init()
+{
+  bmcsd_concurrent_stereo_driver_base::init();
+
+  curve_stereo_.reserve(f_.num_instances());
+  //: Setup nodes that act on multiple frames
+  for (unsigned i=0; i < f_.num_instances(); ++i) {
+    bmcsd_stereo_filter_e *p = new bmcsd_stereo_filter_e();
+    curve_stereo_.push_back(p);
+
+    p->setup_inputs(
+        f_.instance(i),
+        cam_src_,
+        edg_src_,
+        edg_dt_,
+        frag_src_,
+        cvlet_src_,
+        frag_tangents_);
+    
+  }
+  update_stereo_params();
+  init_end();
+  return true;
+}
+
+bool bmcsd_concurrent_stereo_driver_e::
+run(unsigned long timestamp)
+{
+  std::cout << "Running stereo driver_e.\n";
+  assert(initialized_);
+  bprod_signal retval = output_job_->run(timestamp);
+
+  if (retval == BPROD_INVALID)
+    return false;
+  bmcsd_stereo_aggregator *o = dynamic_cast<bmcsd_stereo_aggregator *> (output_job_.ptr());
+  crv3d_ = &o->crv3d_;
+  attr_  = &o->attr_;
+  corresp_  = &o->corresp_;
+  return true;
+}
---
+++ vxd/contrib/brld/bmvgd/bmcsd/pro/bmcsd_stereo_driver_e.h
@@ -0,0 +1,56 @@
+// This is bmcsd_stereo_driver_e.h
+#ifndef bmcsd_stereo_driver_e_h
+#define bmcsd_stereo_driver_e_h
+//:
+//\file
+//\brief Class to run bmcsd_*_curve_stereo in a multiview sequence
+//\author Ricardo Fabbri, Brown University and Rio de Janeiro State U. (rfabbri.github.io)
+//\date 08/12/2009 02:19:28 PM PDT
+//
+
+#include <bmcsd/bmcsd_curve_3d_attributes_e.h>
+#include <bmcsd/bmcsd_curve_3d_sketch_e.h>
+#include <bmcsd/algo/bmcsd_odt_curve_stereo_e.h>
+
+//: Performs multiview stereo on a video sequence, by running many instances of
+// two-view stereo in subsets of the frames.
+class bmcsd_stereo_driver_e : public bmcsd_stereo_driver_base {
+public: 
+  //: The resulting 3D reconstruction after run();
+  void get_curve_sketch(bmcsd_curve_3d_sketch_e *csk) const
+    { csk->set(*crv3d_, *attr_); }
+
+  //: The correspondence structures between the first two views, for each
+  // instance (not necessarily in order of instances when run in parallel, but
+  // you can recover the instance from the bmcsd_curve_3d_attributes).
+  const bmcsd_discrete_corresp_e &corresp(unsigned i) const
+    { return (*corresp_)[i]; }
+
+  unsigned num_corresp() const
+    { return corresp_->size(); }
+
+  //: The number of computed results.
+  unsigned num_outputted_data() const { 
+      assert (corresp_->size() == crv3d_->size()); 
+      assert (attr_->size() == corresp_->size()); 
+      return attr_->size();
+    }
+
+protected:
+  //: Outputs
+  bmcsd_curve_3d_sketch_e csk_;
+  std::vector< bmcsd_curve_3d_attributes_e > *attr_;
+  std::vector<bmcsd_discrete_corresp_e> *corresp_;
+};
+
+class bmcsd_concurrent_stereo_driver_e : public bmcsd_concurrent_stereo_driver_base {
+public:
+  //: Initializes the processing, e.g. setting up a processing graph, computing
+  // tangents in the curve fragments, etc.
+  virtual bool init();
+
+  //: Runs all instances of the 2-view matching and reconstruction
+  virtual bool run(unsigned long timestamp=1);
+};
+
+#endif // bmcsd_stereo_driver_e_h
