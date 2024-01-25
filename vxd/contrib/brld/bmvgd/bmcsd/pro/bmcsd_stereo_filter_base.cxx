483dfb3614f962bf0d1aea26cbffce0806ac57ac
Ricardo Fabbri <rfabbri@gmail.com>
2024-01-25 16:29:30 -0300

    [3DCD] futherred stereo_filter

--- vxd/contrib/brld/bmvgd/bmcsd/pro/bmcsd_stereo_filter_base.cxx
+++ vxd/contrib/brld/bmvgd/bmcsd/pro/bmcsd_stereo_filter_base.cxx
@@ -63,7 +63,6 @@ get_cameras()
 
   assert(input_type_id(CAM_ID0) == typeid(vpgl_perspective_camera<double>));
   cams[0].set_p(input<vpgl_perspective_camera<double> >(CAM_ID0));
-
   assert(input_type_id(CAM_ID1) == typeid(vpgl_perspective_camera<double>));
   cams[1].set_p(input<vpgl_perspective_camera<double> >(CAM_ID1));
 
@@ -88,7 +87,6 @@ get_edgemaps()
     unsigned offset = confirmation_view_input_offset_ 
                     + i*sources_per_confirmation_view_;
     assert( input_type_id(offset + EDG_ID) == typeid(sdet_edgemap_sptr) );
-
     em[i+2] = input< sdet_edgemap_sptr >(offset + EDG_ID);
   }
   s_->set_all_edgemaps(em);
@@ -103,16 +101,36 @@ get_curvelets()
     unsigned offset = confirmation_view_input_offset_ 
                     + i*sources_per_confirmation_view_;
     assert( input_type_id(offset + CVLET_ID) == typeid(sdetd_sel_storage_sptr) );
-
     sels[i+2] = input< sdetd_sel_storage_sptr >(offset + CVLET_ID);
   }
   s_->set_all_sels(sels);
 }
 
 void bmcsd_stereo_filter_base::
+get_dt_label()
+{
+  typedef vil_image_view<vxl_uint_32> dt_t;
+  typedef vil_image_view<unsigned> label_t;
+  std::vector< dt_t > dts(s_.nviews());
+  std::vector< label_t > labels(s_.nviews());
+
+  // dts[0] == null;
+  // labels [0] == null;
+  for (unsigned i=0; i < v_->num_confirmation_views(); ++i) {
+    unsigned offset = confirmation_view_input_offset_ + i*sources_per_confirmation_view_;
+    assert(input_type_id(offset + DT_ID) == typeid( dt_t ));
+    assert(input_type_id(offset + LBL_ID) == typeid( label_t ));
+    dts[i+2] = input< dt_t >(offset + DT_ID);
+    labels[i+2] = input< label_t >(offset + LBL_ID);
+  }
+  s_.set_all_dt_label(dts, labels);
+}
+
+
+void bmcsd_stereo_filter_base::
 get_curves_and_tangents()
 {
-  std::vector<std::vector< vsol_polyline_2d_sptr > > curves (s_->nviews()); 
+  std::vector<std::vector< vsol_polyline_2d_sptr > > curves (s_.nviews()); 
 
   assert(input_type_id(FRAG_ID0) == typeid(std::vector< vsol_polyline_2d_sptr >));
   assert(input_type_id(FRAG_ID1) == typeid(std::vector< vsol_polyline_2d_sptr >));
@@ -143,61 +161,3 @@ get_curves_and_tangents()
   s_.break_into_episegs_and_replace_curve(&sseq);
 }
 
-void bmcsd_stereo_filter_base::
-get_dt_label()
-{
-  typedef vil_image_view<vxl_uint_32> dt_t;
-  typedef vil_image_view<unsigned> label_t;
-  std::vector< dt_t > dts(s_.nviews());
-  std::vector< label_t > labels(s_.nviews());
-
-  // dts[0] == null;
-  // labels [0] == null;
-  for (unsigned i=0; i < v_->num_confirmation_views(); ++i) {
-    unsigned offset = confirmation_view_input_offset_ + i*sources_per_confirmation_view_;
-    assert(input_type_id(offset + DT_ID) == typeid( dt_t ));
-    assert(input_type_id(offset + LBL_ID) == typeid( label_t ));
-
-    dts[i+2] = input< dt_t >(offset + DT_ID);
-    labels[i+2] = input< label_t >(offset + LBL_ID);
-  }
-  s_.set_all_dt_label(dts, labels);
-}
-
-bprod_signal bmcsd_stereo_aggregator::
-run(unsigned long timestamp,
-    bprod_debug_observer* const debug)
-{
-  // notify the debugger if available
-  if (debug) debug->notify_enter(this, timestamp);
-
-  if(!this->enabled()){
-    // notify the debugger if available
-    if (debug) debug->notify_exit(this, timestamp);
-    return BPROD_VALID;
-  }
-  
-  update_mutex_.lock();
-
-  if(timestamp > this->timestamp_){
-    this->timestamp_ = timestamp;
-    this->last_signal_ = this->request_inputs_serial(timestamp,debug);
-    if(this->last_signal_ == BPROD_VALID){
-      if (debug){
-        debug->notify_pre_exec(this);
-        this->last_signal_ = this->execute();
-        debug->notify_post_exec(this);
-      }
-      else
-        this->last_signal_ = this->execute();
-    }
-    this->clear();
-  }
-  
-  update_mutex_.unlock();
-
-  // notify the debugger if available
-  if (debug) debug->notify_exit(this, timestamp);
-
-  return this->last_signal_;
-}
--- vxd/contrib/brld/bmvgd/bmcsd/pro/bmcsd_stereo_filter_e.h
+++ vxd/contrib/brld/bmvgd/bmcsd/pro/bmcsd_stereo_filter_e.h
@@ -34,9 +34,12 @@ public:
 
   bprod_signal execute() override
   {
-    bmcsd_stereo_filter_base::execute();
+    bprod_signal sig = bmcsd_stereo_filter_base::execute();
+    if (sig != BBPROD_VALID)
+      return sig;
 
     std::vector<bdifd_1st_order_curve_3d> crv3d;
+    std::vector< bmcsd_curve_3d_attributes_e > attr;
     bmcsd_discrete_corresp corresp_e;
     // TODO: set inlier views.
     if (!bmcsd_match_and_reconstruct_all_curves_attr_e(s_, &crv3d, &corresp, &attr)) {
@@ -91,10 +94,10 @@ private:
 };
 
 //: Outputs the concatenation of all inputs from many bmcsd_stereo_filter_e
-class bmcsd_stereo_jobs: public bprod_filter {
+class bmcsd_stereo_jobs_e: public bprod_filter {
 public:
 
-  bmcsd_stereo_jobs(unsigned num_matchers) 
+  bmcsd_stereo_jobs_e(unsigned num_matchers) 
     :
     num_matchers_(num_matchers)
   {}
@@ -102,8 +105,8 @@ public:
   bprod_signal execute() 
   {
     std::vector< bdifd_1st_order_curve_3d > crv3d;
-    std::vector< bmcsd_curve_3d_attributes > attr;
-    std::vector<bmcsd_discrete_corresp> corresp;
+    std::vector< bmcsd_curve_3d_attributes_e > attr;
+    std::vector<bmcsd_discrete_corresp_e> corresp;
 
     unsigned num_curves=0;
     for (unsigned i=0; i < num_matchers_; ++i) {
@@ -113,25 +116,21 @@ public:
 
     crv3d.reserve(num_curves);
     attr.reserve(num_curves);
-
     corresp.reserve(num_matchers_);
-
     for (unsigned i=0; i < num_matchers_; ++i) {
-      assert(input_type_id(3*i) == typeid(std::vector< bdifd_1st_order_curve_3d >));
-      assert(input_type_id(3*i+1) == typeid(std::vector< bmcsd_curve_3d_attributes >));
-      assert(input_type_id(3*i+2) == typeid(bmcsd_discrete_corresp));
+      assert(input_type_id(3*i)   == typeid(std::vector< bdifd_1st_order_curve_3d >));
+      assert(input_type_id(3*i+1) == typeid(std::vector< bmcsd_curve_3d_attributes_e >));
+      assert(input_type_id(3*i+2) == typeid(bmcsd_discrete_corresp_e));
       const std::vector< bdifd_1st_order_curve_3d > &crv3d_i 
         = input<std::vector< bdifd_1st_order_curve_3d > >(3*i);
 
-      const std::vector< bmcsd_curve_3d_attributes > attr_i
-        = input<std::vector< bmcsd_curve_3d_attributes> >(3*i + 1);
+      const std::vector< bmcsd_curve_3d_attributes_e > attr_i
+        = input<std::vector< bmcsd_curve_3d_attributes_e > >(3*i + 1);
 
       assert(attr_i.size() == crv3d_i.size());
-
       crv3d.insert(crv3d.end(), crv3d_i.begin(), crv3d_i.end());
       attr.insert(attr.end(), attr_i.begin(), attr_i.end());
-
-      corresp.push_back(input< bmcsd_discrete_corresp >(3*i + 2));
+      corresp.push_back(input< bmcsd_discrete_corresp_e >(3*i + 2));
     }
 
     output(0, crv3d);
@@ -145,44 +144,42 @@ private:
 };
 
 //: Stores the concatenation of all inputs from many bmcsd_stereo_jobs
-class bmcsd_stereo_aggregator : public bprod_sink {
+class bmcsd_stereo_aggregator_e : public bprod_sink {
 public:
 
-  bmcsd_stereo_aggregator(unsigned num_jobs)
+  bmcsd_stereo_aggregator_e(unsigned num_jobs)
     : num_jobs_(num_jobs) { }
 
   bprod_signal execute()
   {
     corresp_.reserve(num_jobs_);
-
     unsigned inputs_per_job = 3;
     unsigned num_curves=0, num_corr=0, num_attribs=0;
     for (unsigned i=0; i < num_jobs_; ++i) {
       assert(input_type_id(inputs_per_job*i) == typeid(std::vector< bdifd_1st_order_curve_3d >));
-      assert(input_type_id(inputs_per_job*i+1) == typeid(std::vector< bmcsd_curve_3d_attributes >));
-      assert(input_type_id(inputs_per_job*i+2) == typeid(std::vector< bmcsd_discrete_corresp >));
-      num_curves += input<std::vector< bdifd_1st_order_curve_3d > >(inputs_per_job*i).size();
-      num_attribs += input<std::vector< bmcsd_curve_3d_attributes > >(inputs_per_job*i+1).size();
+      assert(input_type_id(inputs_per_job*i+1) == typeid(std::vector< bmcsd_curve_3d_attributes_e >));
+      assert(input_type_id(inputs_per_job*i+2) == typeid(std::vector< bmcsd_discrete_corresp_e >));
+      num_curves  += input<std::vector< bdifd_1st_order_curve_3d >  >(inputs_per_job*i).size();
+      num_attribs += input<std::vector< bmcsd_curve_3d_attributes_e > >(inputs_per_job*i+1).size();
       assert (num_curves == num_attribs);
-      num_corr +=  input<std::vector< bmcsd_discrete_corresp > >(inputs_per_job*i + 2).size();
+      num_corr +=  input<std::vector< bmcsd_discrete_corresp_e > >(inputs_per_job*i + 2).size();
     }
 
     crv3d_.reserve(num_curves);
     attr_.reserve(num_curves);
     corresp_.reserve(num_corr);
-
     for (unsigned i=0; i < num_jobs_; ++i) {
       const std::vector< bdifd_1st_order_curve_3d > &crv3d_i 
         = input<std::vector< bdifd_1st_order_curve_3d > >(inputs_per_job*i);
 
-      const std::vector< bmcsd_curve_3d_attributes > &attr_i
-        = input<std::vector< bmcsd_curve_3d_attributes> >(inputs_per_job*i + 1);
+      const std::vector< bmcsd_curve_3d_attributes_e > &attr_i
+        = input<std::vector< bmcsd_curve_3d_attributes_e> >(inputs_per_job*i + 1);
 
       crv3d_.insert(crv3d_.end(), crv3d_i.begin(), crv3d_i.end());
       attr_.insert(attr_.end(), attr_i.begin(), attr_i.end());
 
-      const std::vector< bmcsd_discrete_corresp > &corr_i
-        = input<std::vector<bmcsd_discrete_corresp> > (inputs_per_job*i + 2);
+      const std::vector< bmcsd_discrete_corresp_e > &corr_i
+        = input<std::vector<bmcsd_discrete_corresp_e> > (inputs_per_job*i + 2);
       corresp_.insert(corresp_.end(), corr_i.begin(), corr_i.end());
     }
 
@@ -194,8 +191,8 @@ public:
                    bprod_debug_observer* const debug = NULL);
 
   std::vector< bdifd_1st_order_curve_3d > crv3d_;
-  std::vector< bmcsd_curve_3d_attributes > attr_;
-  std::vector< bmcsd_discrete_corresp > corresp_;
+  std::vector< bmcsd_curve_3d_attributes_e > attr_;
+  std::vector< bmcsd_discrete_corresp_e > corresp_;
 
 private:
   unsigned num_jobs_;
