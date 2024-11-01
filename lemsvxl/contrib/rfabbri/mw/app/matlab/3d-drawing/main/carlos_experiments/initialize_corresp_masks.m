cc9f9c2c1676b5e4c21557a5d947d9ba2c640beb
Carlos Luide Bião dos Reis <clbreis@uesc.br>
2024-11-01 16:45:05 -0300

    [carlos_experiment_folder]

--- lemsvxl/contrib/rfabbri/mw/app/matlab/3d-drawing/main/carlos_experiments/initialize_corresp_masks.m
+++
@@ -1,7 +0,0 @@
-all_corresp_masks = cell(numIM,1);
-for acm=1:numIM
-    if(~isempty(clusters{acm,1}))
-        num_clustered_curves = size(clusters{acm,1},2);
-        all_corresp_masks{acm,1} = cell(1,num_clustered_curves);
-    end
-end
--- lemsvxl/contrib/rfabbri/mw/app/matlab/3d-drawing/main/carlos_experiments/save_curve_drawing_carlostest.m
+++
@@ -1,4 +0,0 @@
-% to be called from _main.m
-write_curve_graph_to_vrml_12carlostest;
-% save where is main file
-save('curve_drawing_pavilion_mixed_half_12.mat','complete_curve_graph','all_junctions');
