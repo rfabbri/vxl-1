d27a26b5050737548bc805a69dd0a7f1235469dd
Carlos Luide Bião dos Reis <clbreis@uesc.br>
2024-12-05 16:35:55 -0300

    [3DCD] easier to run

--- lemsvxl/contrib/rfabbri/mw/app/matlab/3d-drawing/main/save_curve_drawing.m
+++ lemsvxl/contrib/rfabbri/mw/app/matlab/3d-drawing/main/save_curve_drawing.m
@@ -1,4 +1,4 @@
 % to be called from _main.m
 write_curve_graph_to_vrml_12;
 % save where is main file
-save('curve_drawing_pavilion_mixed_half_12.mat','complete_curve_graph','all_junctions');
+save('curve_drawing.mat','complete_curve_graph','all_junctions');
